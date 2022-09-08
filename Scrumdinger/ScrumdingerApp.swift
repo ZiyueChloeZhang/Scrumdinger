d7668277b96017cafe34da4aa482017ad31bb135
Chloe Zhang <chloezhang1030@gmail.com>
2022-09-07 19:21:34 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Chloe Zhang on 2022-09-07.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
